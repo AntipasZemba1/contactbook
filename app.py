346db62423cea6d9d3be7562c1d76bcf43a13f15
Antipas Zemba <antipasnzeba@gmail.com>
2025-06-05 01:21:57 -0700

    Added function to view all contacts

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ def add_contact():
     phone = input("Enter phone number: ") # This will create a disctionary for the contact
     contacts.append(contact) # Add the contact to the list
     print("Contact added!\n") # Confirm addition
+
+# Function to view all contacts
+def view_contacts
+    if not contacts:
+        print("No contacts found.\n") # Inform if list is empty
+    else
+        print("\n📋 All Contacts:")
+        for contact in contacts:
+            print(f"Name: {contact['name']}, Phone: {contact['phone']}")  # Display each contact
+        print()
