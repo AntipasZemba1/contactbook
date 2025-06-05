cb76b8875752ba0a8214da74c238293be118ff84
Antipas Zemba <antipasnzeba@gmail.com>
2025-06-05 01:17:52 -0700

    Function to add a contact

---
+++ app.py
@@ -0,0 +1,9 @@
+# List to store all contacts
+contacts = []
+
+# Function to add a contact
+def add_contact():
+    name = input("Enter name: ") # This will ask user for contact name
+    phone = input("Enter phone number: ") # This will create a disctionary for the contact
+    contacts.append(contact) # Add the contact to the list
+    print("Contact added!\n") # Confirm addition
